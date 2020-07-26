9ed1734222d9e21a4a386ee0581e50a12830f871
antonFJohansson <47623953+antonFJohansson@users.noreply.github.com>
2020-07-26 21:01:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from youtube_class_file import youtube_class
+from LDAmodel_file import LDAmodel
+from retrieve_csv_info_file import retrieve_csv_info
+
+
+## Mike Thurston https://www.youtube.com/channel/UCzGLDaTu81nJDtWK10MniGg
+
+
+
+save_folder = 'downloaded_info'
+video_channel_id = 'UC4ijq8Cg-8zQKx8OH12dUSw'
+num_clusters = 5
+
+
+## Code starts here
+channel_info = youtube_class(save_folder, video_channel_id)
+channel_info.retrieve_channel_info()
+
+sub_store, title_store, video_id_store = retrieve_csv_info('Subs', save_folder)
+
+lda = LDAmodel(sub_store, title_store, video_id_store,
+               num_clusters, num_passes = 100, no_below=10,
+               no_above=0.5)
+lda.fit_LDAmodel()
+lda.print_clusters()
+lda.save_video_clusters(save_folder)
+lda.print_cluster_help()
+
